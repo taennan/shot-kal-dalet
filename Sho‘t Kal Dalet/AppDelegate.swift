19a6f5046c97d3949e58e862f313bd6c09305532
Taennan Rickman <Taennan@imac.local>
2020-09-21 17:03:49 +1200

    Initial Commit

---
+++ Sho‘t Kal Dalet/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Sho‘t Kal Dalet
+//
+//  Created by Taennan Rickman on 21/9/20.
+//  Copyright © 2020 Taennan Rickman. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
